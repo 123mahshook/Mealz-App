0018eee7f2be4a2bc2b3a05cd31ecb2cd21d065b
123mahshook <mahshookrk@gmail.com>
2022-11-28 10:59:13 +0530

    Collapsing toolbar on Column

--- app/src/main/java/com/mahshook/mealzapp/ui/details/MealsDetailsScreen.kt
+++ app/src/main/java/com/mahshook/mealzapp/ui/details/MealsDetailsScreen.kt
@@ -6,105 +6,92 @@ import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.updateTransition
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.Button
-import androidx.compose.material.Card
-import androidx.compose.material.Text
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.max
+import androidx.compose.ui.unit.min
 import coil.compose.AsyncImage
 import coil.compose.rememberAsyncImagePainter
 import coil.compose.rememberImagePainter
 import coil.transform.CircleCropTransformation
 import com.mahshook.model.response.MealsResponse
+import kotlin.math.max
+import kotlin.math.min
 
 @Composable
 fun MealsDetailsScreen(meal: MealsResponse?) {
-    /* var  isExpanded by remember {
-         mutableStateOf(false)
-     }*/
-    var profilePictureState by remember {
-        mutableStateOf(MealProfielPictureState.Normal)
-    }
-    val transition = updateTransition(
-        targetState = profilePictureState,
-        label = ""
-    )
-    /*val imageSizeDp: Dp by animateDpAsState(
-        targetValue =
-        if(isExpanded) 200.dp
-        else 100.dp)*/
-
-    val imageSizeDp by transition.animateDp(
-        targetValueByState = {
-            it.size
-        }, label = ""
-    )
-    val color by transition.animateColor(
-        targetValueByState = {
-            it.color
-        },
-        label = "",
-    )
-
-    val widthSize by transition.animateDp(targetValueByState = {
-        it.borderWidth
-    }, label = "")
-
-    Column() {
-        Row() {
-            Card(
-                modifier =
-                Modifier.padding(
-                    16.dp
-                ),
-                shape = CircleShape,
-                border = BorderStroke(
-                    width = widthSize,
-                    color = color
-                )
+  val scrollState = rememberScrollState()
+  val offset=min(1f,1- (scrollState.value/600f))
+  val size by animateDpAsState(targetValue = max(100.dp,200.dp*offset))
+androidx.compose.material.Surface(
+    color = MaterialTheme.colors.background
+) {
+    Column(
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Surface(elevation = 4.dp) {
+            Row(
+                modifier = Modifier.fillMaxWidth()
             ) {
-                Image(
-                    painter = rememberImagePainter(data = meal?.imageUrl,
-                        builder = {
-                            transformations(CircleCropTransformation())
-                        }),
-                    contentDescription = null,
+                Card(
+                    modifier =
+                    Modifier.padding(
+                        16.dp
+                    ),
+                    shape = CircleShape,
+                    border = BorderStroke(
+                        width = 2.dp,
+                        color = Color.Green
+                    )
+                ) {
+                    Image(
+                        painter = rememberImagePainter(data = meal?.imageUrl,
+                            builder = {
+                                transformations(CircleCropTransformation())
+                            }),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .size(size)
+                    )
+                }
+                Text(
+                    text = meal?.name ?: "default name",
                     modifier = Modifier
-                        .size(imageSizeDp)
-                        .padding(8.dp)
+                        .padding(16.dp)
+                        .align(Alignment.CenterVertically)
                 )
+
             }
-            Text(
-                text = meal?.name ?: "default name",
-                modifier = Modifier
-                    .padding(16.dp)
-                    .align(Alignment.CenterVertically)
-            )
-
         }
 
-        Button(
-            modifier = Modifier.padding(16.dp),
-            onClick = {
-                //isExpanded = !isExpanded
-                profilePictureState =
-                    if (profilePictureState == MealProfielPictureState.Normal)
-                        MealProfielPictureState.Expanded
-                    else
-                        MealProfielPictureState.Normal
-            }) {
-            Text(text = "Change state of meal profile picture")
+        Column(
+            modifier = Modifier.verticalScroll(scrollState)
+        ) {
+            Text("This is a text element", modifier = Modifier.padding(32.dp))
+            Text("This is a text element", modifier = Modifier.padding(32.dp))
+            Text("This is a text element", modifier = Modifier.padding(32.dp))
+            Text("This is a text element", modifier = Modifier.padding(32.dp))
+            Text("This is a text element", modifier = Modifier.padding(32.dp))
+            Text("This is a text element", modifier = Modifier.padding(32.dp))
+            Text("This is a text element", modifier = Modifier.padding(32.dp))
+            Text("This is a text element", modifier = Modifier.padding(32.dp))
+            Text("This is a text element", modifier = Modifier.padding(32.dp))
+            Text("This is a text element", modifier = Modifier.padding(32.dp))
         }
+
+        
     }
+}
+    
 
 }
 
